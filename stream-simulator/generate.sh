9344ad7dcb0c9bfc291f045748d2cfaa1b990ac5
James <jmedel@hortonworks.com>
2018-01-18 15:24:42 -0800

    update generate.sh

--- stream-simulator/generate.sh
+++ stream-simulator/generate.sh
@@ -1,1 +1,1 @@
-java -Xmx1024m -jar ~/iot-truck-streaming/stream-simulator/target/stream-simulator.jar 6 250 com.hortonworks.streaming.impl.domain.transport.Truck com.hortonworks.streaming.impl.collectors.StdOutEventCollector 1000 /etc/storm_demo/routes/midwest 1000
+java -Xmx1024m -jar /home/nifi/iot-truck-streaming/stream-simulator/target/stream-simulator.jar 6 250 com.hortonworks.streaming.impl.domain.transport.Truck com.hortonworks.streaming.impl.collectors.StdOutEventCollector 1000 /etc/storm_demo/routes/midwest 1000
